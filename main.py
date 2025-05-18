a78792f4edf851acaab1076f21b7b2201423b112
bulat <nibulat924@gmail.com>
2025-05-18 16:02:57 +0400

    test ci

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from contextlib import asynccontextmanager
 from utils import consume_message
 from fastapi import FastAPI
-from config import Settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    settings = Settings()
     print(1)
     await consume_message()
     yield
